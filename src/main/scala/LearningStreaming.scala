48e12a0fdc5fe5ecf179b8428d6d6a9ae5d93ae1
rodrigo zembrzuski <rodrigo.zembrzuski@gruporbs.com.br>
2017-02-01 16:48:51 -0200

    spark streaming - beabah
    contando numero de visitas por materia em cada hora

--- src/main/scala/LearningStreaming.scala
+++ src/main/scala/LearningStreaming.scala
@@ -1,24 +1,28 @@
 import org.apache.spark.streaming.{Seconds, StreamingContext}
 import org.apache.spark.{SparkConf, SparkContext}
 
-/**
-  * Created by rodrigoz on 2/1/17.
-  */
 object LearningStreaming {
 
   def main(args: Array[String]): Unit = {
+    // nc -lk 7777
+    // materia1 - 12:35
 
     val sc = new SparkContext(new SparkConf().setAppName("hello-spark").setMaster("local[*]"))
     val ssc = new StreamingContext(sc, Seconds(5))
     ssc.checkpoint("/home/rodrigoz/studying-area/spark-checkpoint")
+
     val lines = ssc.socketTextStream("localhost", 7777)
-    val responseCodeDStream = lines.map(x => (x, 1L))
+    val responseCodeDStream = lines.map { x =>
+      val splitted = x.split(" - ")
+      val nomeMateria = splitted(0)
+      val hora = splitted(1).split(":")(0)
+      ((nomeMateria, hora), 1L)
+    }
     val responseCodeCountDStream = responseCodeDStream.updateStateByKey(updateRunningSum _)
     responseCodeCountDStream.foreachRDD(x => x.foreach(x => println(x)))
 
     ssc.start()
     ssc.awaitTermination()
-
   }
 
 
